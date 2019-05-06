d05e19edba9f15bcb47c4c48e2c7e764c548ae26
aqsaanamuaf <aqsaanamciitswl@gmail.com>
2019-05-06 22:13:03 +0500

    program file add

--- Initialize - Copy.java
+++
@@ -1,9 +0,0 @@
-public class Initialize  
-{     
-public static void main(String[] args)     
-{        
-int month = 2, days = 28;         
-System.out.println("Month " + month + " has " +                          
- days + " days.");
-}
-} 

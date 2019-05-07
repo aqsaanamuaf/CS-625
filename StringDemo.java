8cf242869696b3c85744f0c2c5414d745dc696b1
aqsaanamuaf <aqsaanamciitswl@gmail.com>
2019-05-07 15:13:22 +0500

    Add commit file

---
+++ StringDemo.java
@@ -0,0 +1,12 @@
+class StringDemo {
+public static void main(String args[]) 
+{
+String Str1 = new String("Java string are objects.");
+String Str2 ="They are constructed various ways.";
+String Str3 = new String (Str2);
+cd 
+System.out.println(Str1);
+System.out.println(Str2);
+System.out.println(Str3);
+}
+}
